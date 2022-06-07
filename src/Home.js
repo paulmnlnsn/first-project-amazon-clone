5025a0f6de10c19f1bff16a131d439ae1a5094b8
John Paul <johnpaulmanalansan1@gmail.com>
2022-06-07 14:53:38 +0800

    try

--- src/Home.js
+++ src/Home.js
@@ -18,7 +18,7 @@ function Home() {
             title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
             price={11.96}
             rating={5}
-            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
+            
           />
           <Product
             id="49538094"
