8462712aaf312ea60b80468ac031239f438b0821
John Paul <johnpaulmanalansan1@gmail.com>
2022-06-07 15:34:50 +0800

    edit

--- src/Home.js
+++ src/Home.js
@@ -15,7 +15,7 @@ function Home() {
         <div className="home__row">
           <Product
             id="12321341"
-            title="The Lean : How Constant Innovation Creates Radically Successful Businesses Paperback"
+            title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
             price={11.96}
             rating={5}
             image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
