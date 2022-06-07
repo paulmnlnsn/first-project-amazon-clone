255757c22948d41b09822468d76ac96aec9b3579
John Paul <johnpaulmanalansan1@gmail.com>
2022-06-07 14:41:55 +0800

    second commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,15 @@
 import firebase from "firebase";
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB_sLyHIwLDh2YOEbBdCjdxNTO_VhyMS88",
-  authDomain: "challenge-51736.firebaseapp.com",
-  projectId: "challenge-51736",
-  storageBucket: "challenge-51736.appspot.com",
-  messagingSenderId: "24536927903",
-  appId: "1:24536927903:web:43c44e1b39613aedf7dcaa",
-  measurementId: "G-EZL4RDHKYZ"
+  apiKey: "AIzaSyCcPSKlYtpdzBoAC8soeSmIARMzVKzrf5I",
+  authDomain: "challenge-4b2b2.firebaseapp.com",
+  databaseURL: "https://challenge-4b2b2.firebaseio.com",
+  projectId: "challenge-4b2b2",
+  storageBucket: "challenge-4b2b2.appspot.com",
+  messagingSenderId: "962418448875",
+  appId: "1:962418448875:web:f6cce5eeaf819481f661ae",
 };
+
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 const db = firebaseApp.firestore();
